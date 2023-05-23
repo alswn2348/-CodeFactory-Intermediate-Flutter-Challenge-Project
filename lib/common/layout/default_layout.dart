6aa2a23045954cb3f9f78a2a38b9cc6e8832fdf9
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-05-23 17:22:05 +0900

    design : tabbar

--- lib/common/layout/default_layout.dart
+++ lib/common/layout/default_layout.dart
@@ -1,20 +1,42 @@
+import 'package:codefactory_flutte_project/common/const/sizes.dart';
 import 'package:flutter/material.dart';
 
 class DefaultLayout extends StatelessWidget {
   final Color? backgroundColor;
   final Widget child;
+  final String? title;
 
   const DefaultLayout({
     super.key,
     required this.child,
     this.backgroundColor,
+    this.title,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor ?? Colors.white,
+      appBar: randerAppBar(),
       body: child,
     );
+  }
+
+  AppBar? randerAppBar() {
+    if (title != null) {
+      return AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        foregroundColor: Colors.black,
+        title: Text(
+          title!,
+          style: const TextStyle(
+            fontSize: Sizes.size16,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+      );
+    }
+    return null;
   }
 }
--- lib/common/view/root_tab.dart
+++ lib/common/view/root_tab.dart
@@ -1,12 +1,50 @@
+import 'package:codefactory_flutte_project/common/const/colors.dart';
 import 'package:codefactory_flutte_project/common/layout/default_layout.dart';
 import 'package:flutter/material.dart';
 
-class RootTab extends StatelessWidget {
+class RootTab extends StatefulWidget {
   const RootTab({super.key});
+
+  @override
+  State<RootTab> createState() => _RootTabState();
+}
+
+class _RootTabState extends State<RootTab> {
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
+      title: "딜리버리 타이틀 연습",
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: PRIMARY_COLOR,
+        unselectedItemColor: BODY_TEXT_COLOR,
+        selectedFontSize: 10,
+        unselectedFontSize: 10,
+        type: BottomNavigationBarType.fixed, // shifting 은 선택 강조
+        onTap: (value) {
+          index = value;
+        },
+        currentIndex: index,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            label: '홈',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.fastfood_outlined),
+            label: '음식',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.receipt_long_outlined),
+            label: '주문',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            label: '프로필',
+          ),
+        ],
+      ),
       child: Container(
         child: const Text("root tab"),
       ),
