fce9b0a82f470f066db7c5e56167696fa1371197
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-05-24 18:24:33 +0900

    Update restaurant_screen.dart

--- lib/restaurant/view/restaurant_screen.dart
+++ lib/restaurant/view/restaurant_screen.dart
@@ -1,15 +1,30 @@
+import 'package:codefactory_flutte_project/restaurant/component/restaurant_card.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 
-class RastaurantScreen extends StatelessWidget {
-  const RastaurantScreen({super.key});
+class RestaurantScreen extends StatelessWidget {
+  const RestaurantScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: const Center(
-        child: Text("홈"),
+      child: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: RestaurantCard(
+            image: Image.asset(
+              'asset/img/food/ddeok_bok_gi.jpg',
+              fit: BoxFit.cover,
+            ),
+            name: '엽기 떡볶이',
+            tags: const ['떡볶이', '치즈', '매운맛'],
+            ratingCount: 100,
+            deliveryTime: 15,
+            deliveryFee: 2000,
+            rating: 4.52,
+          ),
+        ),
       ),
     );
   }
