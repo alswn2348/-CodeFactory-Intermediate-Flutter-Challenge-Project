61e8d412c6ad279283c71753ca1a0ba3a5b17c39
minju kim <62362753+alswn2348@users.noreply.github.com>
2023-05-31 00:30:43 +0900

    design : detail card list

--- lib/restaurant/view/restaurant_detail_screen.dart
+++ lib/restaurant/view/restaurant_detail_screen.dart
@@ -9,30 +9,62 @@ class RestaurantDetailScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultLayout(
-      title: "떡뽂이",
-      child: Column(
-        children: [
-          RestaurantCard(
-            image: Image.asset(
-              'asset/img/food/ddeok_bok_gi.jpg',
-              fit: BoxFit.cover,
-            ),
-            name: '엽기 떡볶이',
-            tags: const ['떡볶이', '치즈', '매운맛'],
-            ratingsCount: 100,
-            deliveryTime: 15,
-            deliveryFee: 2000,
-            ratings: 4.52,
-            isDetail: true,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 16.0,
-            ),
-            child: ProductCard(),
-          ),
-        ],
+        title: "떡뽂이",
+        child: CustomScrollView(
+          slivers: [
+            randerTop(),
+            randerLabel(),
+            randerProducts(),
+          ],
+        ));
+  }
+
+  SliverToBoxAdapter randerTop() {
+    return SliverToBoxAdapter(
+      child: RestaurantCard(
+        image: Image.asset(
+          'asset/img/food/ddeok_bok_gi.jpg',
+          fit: BoxFit.cover,
+        ),
+        name: '엽기 떡볶이',
+        tags: const ['떡볶이', '치즈', '매운맛'],
+        ratingsCount: 100,
+        deliveryTime: 15,
+        deliveryFee: 2000,
+        ratings: 4.52,
+        isDetail: true,
       ),
     );
+  }
+
+  SliverPadding randerProducts() {
+    return SliverPadding(
+      padding: const EdgeInsets.symmetric(horizontal: 16.0),
+      sliver: SliverList(
+        delegate: SliverChildBuilderDelegate(
+          childCount: 10,
+          (context, index) {
+            return const Padding(
+              padding: EdgeInsets.only(top: 16.0),
+              child: ProductCard(),
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  SliverPadding randerLabel() {
+    return const SliverPadding(
+        padding: EdgeInsets.symmetric(horizontal: 16.0),
+        sliver: SliverToBoxAdapter(
+          child: Text(
+            "메뉴",
+            style: TextStyle(
+              fontSize: 18.0,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+        ));
   }
 }
